9d410a916ced6681e250dbfd5ed7ebdece7eef68
Juanjo <juanjose.ortiz@elespanol.com>
2019-09-23 10:30:23 +0200

    fix component layout+

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "./Layout";
+
+
+
+const App = (props) => {
+
+    return(
+        <Layout>
+            Test
+        </Layout>
+    )
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Layout from './Layout';
+import App from './App';
 
 ReactDOM.render(
-      <Layout />
+      <App />
     , document.getElementById("app"));
 
